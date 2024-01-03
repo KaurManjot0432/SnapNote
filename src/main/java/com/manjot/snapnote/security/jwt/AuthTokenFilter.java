c230a9e57e8fc21455e18a7476cf5ba039c3b70a
KaurManjot0432 <0432manjot@gmail.com>
2024-01-03 15:05:25 +0530

    adding userName in request attributes

--- src/main/java/com/manjot/snapnote/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/manjot/snapnote/security/jwt/AuthTokenFilter.java
@@ -43,6 +43,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+                request.setAttribute("userName", username);
             }
         } catch (Exception e) {
             logger.error("Cannot set user authentication: {}", e);
