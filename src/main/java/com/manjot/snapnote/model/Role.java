3b89a3c3d21cd1558c1acb48e037610f926ea4fd
KaurManjot0432 <0432manjot@gmail.com>
2024-01-03 12:28:40 +0530

    added User and Role models

---
+++ src/main/java/com/manjot/snapnote/model/Role.java
@@ -0,0 +1,28 @@
+package com.manjot.snapnote.model;
+
+import com.manjot.snapnote.model.enums.ERole;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.Builder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Document(collection = "role")
+public class Role {
+
+    @Id
+    private String id;
+
+    private ERole name;
+
+}
---
+++ src/main/java/com/manjot/snapnote/model/User.java
@@ -0,0 +1,59 @@
+package com.manjot.snapnote.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Document(collection = "user")
+public class User {
+
+    @Id
+    private String id;
+
+    @NotNull
+    @Indexed(unique = true)
+    private String username;
+
+    @NotNull
+    @Email
+    @Indexed(unique = true)
+    private String email;
+
+    @NotNull
+    @Size(min = 5)
+    private String password;
+
+    @DBRef
+    private Set<Role> roles = new HashSet<>();
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
