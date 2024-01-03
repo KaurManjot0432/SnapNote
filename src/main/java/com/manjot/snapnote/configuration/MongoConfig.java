6cb1864afadab46f550400f40b8f0b1374b74879
KaurManjot0432 <0432manjot@gmail.com>
2024-01-03 15:04:30 +0530

    added mongodb config

---
+++ src/main/java/com/manjot/snapnote/configuration/MongoConfig.java
@@ -0,0 +1,10 @@
+package com.manjot.snapnote.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+    // additional configuration if needed
+}
